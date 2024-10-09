f0fee74df97d474ba17b4c8a4af2686e9662c128
Andy <greenyplay81@gmail.com>
2024-10-09 09:38:02 +0300

    New DZ

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Andrey'
+print('Name: '+ name)
+
+age = 18
+print('Age: ' + str(age))
+age = age + 2
+print('New Age: ' + str(age))
+
+is_student = True
+print('Is Student: ' + str(is_student))
+
+
