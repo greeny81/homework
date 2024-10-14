9a725ed68aa8c097b0bf870cad2b061260c7d92d
Andy <greenyplay81@gmail.com>
2024-10-14 15:22:33 +0300

    Leson_1_5 add comm

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = tuple((1,2,3,True,[10,20]))
 #print(type(immutable_var))
-print(immutable_var)
+print("immutable_var:",immutable_var)
 
 #immutable_var[3] = False
 #immutable_var[3] = False TypeError: объект кортеж не поддерживает назначение элементов
@@ -9,4 +9,4 @@ mutable_list = ["apple", "banan", "coconut", False, 2, "String"]
 mutable_list[2] = "Клубника"
 mutable_list.append("55")
 mutable_list.remove(mutable_list[1])
-print(mutable_list)
+print("mutable_list:",mutable_list)
