42765ddd750d13f35348cdd4e6fee0c5d51323b8
Andy <greenyplay81@gmail.com>
2024-11-13 11:18:09 +0300

    ch main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+list = ['f','ff','dfd']
+list2 = [2,3,5,6,1]
+sum_ = 0
+for i in range(len(list2)):
+    #list[i] = 'ffffffff'
+    sum_ += list2[i]
+
+#print(list)
+print (sum_)
