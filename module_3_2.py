61e246cb8ec03d526810be9ab14f62e68bc28d2e
Andy <greenyplay81@gmail.com>
2024-11-14 14:47:56 +0300

    3.2

---
+++ module_3_2.py
@@ -0,0 +1,23 @@
+from xml.sax.handler import property_interning_dict
+
+
+def send_email(message, recipient, *, sender = "university.help@gmail.com"):
+    if recipient.find("@") == -1 or sender.find("@") == -1 :
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+
+    if recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    if sender == "university.help@gmail.com":
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+
+
+
+#text_ = "sender"
+#print(text_[-2:len(text_)])
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
