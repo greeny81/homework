ac0cca4b4bd310050c96eb1ccba4b01f298a3e28
Andy <greenyplay81@gmail.com>
2024-11-14 11:10:22 +0300

    KRESTIKI

--- test_2_5.py
+++ test_2_5.py
@@ -1,9 +1,25 @@
-value = 434
-value_ = 333
-matrix = []
-matrix.append([])
-matrix[0].append(value)
-matrix[0].append(value_)
+def draw_area():
+    for i in area:
+        print(*i)
 
+area = [["*","*","*"], ["*","*","*"], ["*","*","*"]]
 
-print(matrix)
+for turn in range(1, 10):
+    print(f'Xod {turn}')
+    if turn % 2 == 0:
+        turn_char = "0"
+    else:
+        turn_char = "X"
+    row = int(input("Numb stroki 1,2,3")) - 1
+    column = int(input("Numb stolb 1,2,3")) - 1
+    if area[row][column] == "*":
+        area[row][column] = turn_char
+    else:
+        print ("Propusk hoda")
+        draw_area()
+        continue
+    draw_area()
+    if area[0][0] == "X" and area[0][1] == "X" and area[0][2] == "X":
+        print ("Krestiki WIN")
+        break
+
