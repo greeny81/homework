d4bde3d1822baa564d37a654da275933c4a81580
Andy <greenyplay81@gmail.com>
2024-11-18 12:39:32 +0300

    mod33

--- main.py
+++ main.py
@@ -7,7 +7,42 @@
 #
 # print(list)
 # print (sum_)
+def print_names2(a, b, c):
+    print(a, b, c)
 
+def print_names3(**kwargs):
+    print(kwargs)
+    for key, value in kwargs.items():
+        print(key, value)
+
+list_ = [1, 2, 3]
+print_names2(list_, 2, 3)
+print_names2(*list_)
+
+list_2 = [1, 3]
+print_names2(7, *list_2)
+print_names2(*list_2, 55)
+
+dict_ = {'a' : 11, 'b' : 22, 'c': 33}
+dict_2 = {'a': 11, 'b': 22, 'd': 44} # ERROR 'd'
+print_names2(**dict_)
+
+print_names3(**dict_2)
+
+lst = [1, 2]
+intc = {'c': 33}
+
+print_names2(*lst, intc)
+print_names2(*lst, **intc)
+
+exit()
+def print_names(*params): # *args, **kwargs
+    print(params)
+    print(*params)
+print_names(1, 2, 4 ,6)
+
+
+exit()
 
 sender = "university.help@gmail.com"
 rec = "urban.teacher@mail.uk"
---
+++ module_3_3.py
@@ -0,0 +1,12 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+# print_params()
+# print_params(b = 25)
+# print_params(c = [1,2,3])
+
+values_list = [33, True, 'werwer']
+values_dict = {'a': 44, 'b' : 55, 'c': 66}
+
+print_params(*values_list)
+print_params(**values_dict)
