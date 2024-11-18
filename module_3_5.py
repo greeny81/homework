c506bd924ad8cdc6c01fb12e25dc2caa6fa91165
Andy <greenyplay81@gmail.com>
2024-11-18 16:17:42 +0300

    3_5

---
+++ module_3_5.py
@@ -0,0 +1,12 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    #print(type(str_number))
+    first = int(str_number[0])
+    #print(first)
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
+print(result)
