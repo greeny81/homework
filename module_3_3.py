4f29e51408fcb502bf5156e63f8bbb1e814ad824
Andy <greenyplay81@gmail.com>
2024-11-18 14:23:21 +0300

    3_3

--- module_3_3.py
+++ module_3_3.py
@@ -4,9 +4,12 @@ def print_params(a = 1, b = 'строка', c = True):
 # print_params()
 # print_params(b = 25)
 # print_params(c = [1,2,3])
-
 values_list = [33, True, 'werwer']
-values_dict = {'a': 44, 'b' : 55, 'c': 66}
+values_dict = {'a': 'qwerty', 'b' : 55, 'c': 66}
 
-print_params(*values_list)
-print_params(**values_dict)
+# print_params(*values_list)
+# print_params(**values_dict)
+
+values_list_2 = ['ffff', 34]
+
+print_params(*values_list_2, 42)
