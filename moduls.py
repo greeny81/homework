fb31be8cbc11c22be7d6bd5f1cd4d62074bf9ad6
Andy <greenyplay81@gmail.com>
2024-11-20 17:01:55 +0300

    m_4_1

---
+++ moduls.py
@@ -0,0 +1,11 @@
+#import moduls1 as m2 #
+#from moduls1 import * #Глобальное обьявление
+from moduls1 import say_hi as sh #Глобальное обьявление
+#print(dir(m2))
+
+print("Привет я из модуля moduls")
+#print(m2.a)
+#m2.say_hi()
+#say_hi()
+
+sh()
---
+++ moduls1.py
@@ -0,0 +1,11 @@
+def say_hi():
+    print("Hi Я из функции во 2м модуле")
+
+def main():
+    a = 5
+    b = 10
+
+print("я moduls1")
+
+if __name__ == '__main__':
+    main()
