615bb9d12b207733e633593b6294bda27aedc6d3
Andy <greenyplay81@gmail.com>
2024-11-20 15:02:57 +0300

    calc

--- calc.py
+++ calc.py
@@ -1,19 +1,45 @@
 import tkinter as tk
 
+def get_values():
+    num1 = int(number1_entry.get())
+    num2 = int(number2_entry.get())
+    return num1, num2
+def insert_values(val):
+    answer_entry.delete(0, 'end')
+    answer_entry.insert(0, val)
 
+def add():
+    num1, num2 = get_values()
+    res = num1 + num2
+    insert_values(res)
+
+def sub():
+    num1, num2 = get_values()
+    res = num1 - num2
+    insert_values(res)
+
+def div():
+    num1, num2 = get_values()
+    res = num1 / num2
+    insert_values(res)
+
+def mul():
+    num1, num2 = get_values()
+    res = num1 * num2
+    insert_values(res)
 
 window = tk.Tk() # создание окна
 window.title("CALCUL")
 window.geometry("300x210")
 window.resizable(False, False)
 
-button_add = tk.Button(window, text="+", width=2, height=1,font='Times 30' )
+button_add = tk.Button(window, text="+", width=2, height=1,font='Times 30', command=add )
 button_add.place(x=10, y=70)
-button_sub = tk.Button(window, text="-", width=2, height=1,font='Times 30' )
+button_sub = tk.Button(window, text="-", width=2, height=1,font='Times 30', command=sub )
 button_sub.place(x=80, y=70)
-button_mul = tk.Button(window, text="*", width=2, height=1,font='Times 30' )
+button_mul = tk.Button(window, text="*", width=2, height=1,font='Times 30', command=mul )
 button_mul.place(x=150, y=70)
-button_div = tk.Button(window, text="/", width=2, height=1,font='Times 30' )
+button_div = tk.Button(window, text="/", width=2, height=1,font='Times 30', command=div )
 button_div.place(x=220, y=70)
 
 number1_entry = tk.Entry(window, width=30)
