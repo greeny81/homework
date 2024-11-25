d1ba2e47bf9ea7f29f2f969dbd24bcad70eb07cc
Andy <greenyplay81@gmail.com>
2024-11-25 10:50:56 +0300

    mod_5_2

--- class_objects.py
+++ class_objects.py
@@ -4,6 +4,12 @@ class Human:
         self.age =  age
         self.say_info()
 
+    def __del__(self): # Срабатывает по завершении кода
+        print(f'{self.name} deleted')
+
+    def __len__(self):
+        return self.age
+
     def say_info(self):
         print(f'Меня зовут {self.name} , мне {self.age} лет')
 
@@ -14,8 +20,11 @@ class Human:
 den = Human('Den', 21)
 max = Human('Max', 25)
 den.surname = "Иванов"
-
-den.birthday()
+den.say_info()
+#del den
+max.birthday()
+print(len(den))
+#input()
 #print(den.name, den.surname, den.age)
 #print(max.name, max.age)
 
