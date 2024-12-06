1e0780495812ada1d90c6101577f0e7ef77fea5d
Andy <greenyplay81@gmail.com>
2024-12-06 10:07:10 +0300

    7_dz2_ready

--- module_7/7_open_files.py
+++ module_7/7_open_files.py
@@ -1,24 +1,40 @@
+import io
 from pprint import pprint
 
 f_name = 'sample2.txt'
-file = open(f_name, 'r') # r , w, a,  append + b- binar file
+# file = open(f_name, 'r') # r , w, a,  append + b- binar file
+# pprint(file.read())
+# file.close()
+#
+# file = open(f_name, 'w')
+# #file.read()  ERROR mode 'w'
+# file.write('Hello world dsfgfdg')
+# file.close()
+#
+# file = open(f_name, 'a')
+# file.write('\nnew text')
+# file.close()
+# file = open(f_name, 'r')
+# print(file.tell())# возвращает положение курсора 0
+# pprint(file.read())# Курсор переместится в конец
+# print(file.tell())# 29
+# pprint(file.read())# Вернет пустоту
+# print(file.seek(15))# Переместит курсор на 15 позицию
+# pprint(file.read())#Прочитает остаток 15+
+# print(file.tell())# 29
+# file.close()
+
+file = open(f_name, 'r', encoding='utf-8')
+print(file.writable()) #Проверка файла на возможность записи , чтения , поиска
+print(file.readable()) #Проверка файла на возможность записи , чтения , поиска
+print(file.seekable()) #Проверка файла на возможность записи , чтения , поиска
+print(file.buffer) #Просмотр различных параметров print(file. ..........)
+print(file.tell())
 pprint(file.read())
-file.close()
+print(file.tell())
+# file.seek(15)
+# file.write('new text')
+# print(file.tell())
 
-file = open(f_name, 'w')
-#file.read()  ERROR mode 'w'
-file.write('Hello world dsfgfdg')
-file.close()
 
-file = open(f_name, 'a')
-file.write('\nnew text')
-file.close()
-
-file = open(f_name, 'r')
-print(file.tell())# возвращает положение курсора 0
-pprint(file.read())# Курсор переместится в конец
-print(file.tell())# 29
-pprint(file.read())# Вернет пустоту
-print(file.seek(15))# Переместит курсор на 15 позицию
-pprint(file.read())#Прочитает остаток 15+
 file.close()
