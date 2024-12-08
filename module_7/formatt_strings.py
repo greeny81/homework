6719c5a44110878a19f0bfd19f61e2369378378b
Andru <greenyplay81@gmail.com>
2024-12-08 18:31:49 +0300

    form_str

---
+++ module_7/formatt_strings.py
@@ -0,0 +1,9 @@
+print('Hello ' + 'world')
+print('My name %s' % 'Andy')
+print('My name %s, im %s' % ('Andy', 'worker'))
+print('My name %(name)s, im %(work)s' % {'name':'Andy', 'work':'worker'})
+print('im working in {}'.format('BTK'))
+print('im working in {} {}'.format('BTK', 'Balaha'))
+
+
+
