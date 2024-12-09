1b2a4218ba020a3cdd7fd0ff27807243cb7200a7
Andru <greenyplay81@gmail.com>
2024-12-09 08:04:38 +0300

    7_form_str

--- module_7/formatt_strings.py
+++ module_7/formatt_strings.py
@@ -4,6 +4,8 @@ print('My name %s, im %s' % ('Andy', 'worker'))
 print('My name %(name)s, im %(work)s' % {'name':'Andy', 'work':'worker'})
 print('im working in {}'.format('BTK'))
 print('im working in {} {}'.format('BTK', 'Balaha'))
-
+print('im working in {0} {1} {0}'.format('BTK', 'Balaha'))
+print('im working in {title} {post}'.format(title='BTK', post='Balaha'))
+print(f'{"любой код"} {str(8*6) + " wer"}')
 
 
