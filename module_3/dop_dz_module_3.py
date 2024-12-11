166153ba47f25a8b2594e4d4cf0dde4ef99a8537
Andy <greenyplay81@gmail.com>
2024-12-11 15:21:44 +0300

    WORKING *_ module_3

--- module_3/dop_dz_module_3.py
+++ module_3/dop_dz_module_3.py
@@ -5,7 +5,7 @@
 data_structure = [[1, 2, 3], # 6
                   {'a': 4, 'b': 5}, # 17
                   (6, {'cube': 7, 'drum': 8}), # 23 , 34, 46
-                  "Hello",
+                  "Hello", # 51
                   ((), [{(2, 'Urban', ('Urban2', 35))}])
                   ]
 
@@ -14,44 +14,64 @@ summ = 0
 def calculate_structure_sum(obj_):
     global summ
     i = 0
-    print(f'!Inputobj_: {str(obj_)}')
+    print(f'\n!!!!! Input-obj_: {str(obj_)} {type(obj_)}')
 
-    #while i <  len(obj_):
     if len(obj_):
-        #print(obj_[i], type(obj_[i]))
+
         if isinstance(obj_[i], list) :
-            #print(obj_[i])
+            print(f'Input List block {str(obj_[i])}')
             summ += sum(obj_[i])
             del(obj_[i])
             print(f'Summ list: {summ}')
+            print(f'Out LIST {str(obj_)}')
+            calculate_structure_sum(obj_)
 
         if isinstance(obj_[i], dict):
+            print(f'Input dict block {str(obj_[i])}')
 
             for key in obj_[i]:
-                print(f'Input dict {str(obj_[i])}')
+
                 summ += len(key) + obj_[i][key]
-                print(f'+dict:{summ}')
+                print(f'Summ+dict:{summ}')
+            print(f'After dict {str(obj_[i])} Summ: {summ}')
             del(obj_[i])
+            print(f'Out DICT {str(obj_)}')
+            calculate_structure_sum(obj_)
 
         if isinstance(obj_[i], str):
-            print(f'STR: {obj_[i]}')
+            print(f'Input STR block: {obj_[i]}')
             summ += len(obj_[i])
             print(f'\nstr:{summ}')
             del(obj_[i])
+            print(f'Out STR {str(obj_)}')
+            calculate_structure_sum(obj_)
 
         if isinstance(obj_[i], tuple):
             tmpLIst = list(obj_[i])
-            print(f'TuptoList: {tmpLIst}')
+            print(f'Input Tuple block: {tmpLIst}')
             j = 0
             while j < len(tmpLIst):
-                print(f'Tuple {j} {tmpLIst[j]} |{tmpLIst} {type(tmpLIst[j])}|')
+                print(f'Tuple obj_{j}={tmpLIst[j]} | <--- {tmpLIst} {type(tmpLIst[j])} {len(tmpLIst[j])} |')
                 if isinstance(tmpLIst[j], int):
                     summ += tmpLIst[j]
                     del(tmpLIst[j])
+                if isinstance(tmpLIst[j], tuple):
+                    for key, val in tmpLIst[j].items():
+                        if isinstance(key, str):
+                            summ += len(key)
 
+
+
+
+                print('Tuple after DEL:' + str(tmpLIst))
                 j += 1
-            obj_[i] = tuple(tmpLIst)
+                obj_[i] = tuple(tmpLIst)
+                print(f'Summ tuple: {summ}')
+
             if len(obj_[i]):
+                print('NOW obj_[i]: ' + str(obj_[i]))
+                print('NOW obj_: ' + str(obj_))
+            #else:
                 calculate_structure_sum(obj_)
 
         #i += 1
--- module_5/5_dop_practice_utube.py
+++ module_5/5_dop_practice_utube.py
@@ -48,3 +48,5 @@ class UrTube:
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+print(vars(v2))
