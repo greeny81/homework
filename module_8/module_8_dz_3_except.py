6f7868a1c981451230044865348f7dc665c296bf
greeny81 <greenyplay81@gmail.com>
2024-12-15 12:26:52 +0300

    8_dz_3

--- module_8/module_8_dz_3_except.py
+++ module_8/module_8_dz_3_except.py
@@ -5,18 +5,21 @@ class IncorrectVinNumber(Exception):
 class Car:
     def __init__(self, model, vin, num):
         if self.__is_valid_vin(vin):
+            print(self.__is_valid_vin(vin))
             self.__vin = vin
         self.__numbers = num
         self.model = model
 
-    def __is_valid_vin(vin_number):
-        if isinstance(vin_number, int) and 1000000 <= vin_number <= 9999999:
-            return True
+    def __is_valid_vin(self,vin_number):
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+        elif vin_number < 1000000 or vin_number > 9999999:
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
         else:
-            pass
+            return True
 
 try:
-  first = Car('Model1', 10000000, 'f123dj')
+  first = Car('Model1', 1000, 'f123dj')
 except IncorrectVinNumber as exc:
   print(exc.message)
 else:
