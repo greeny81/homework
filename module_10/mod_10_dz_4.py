7bc844212bbfc34c4cb70f804a3b82feeaf261df
greeny81 <greenyplay81@gmail.com>
2025-01-02 17:24:00 +0300

    mod_10_dz_4_work

--- module_10/mod_10_dz_4.py
+++ module_10/mod_10_dz_4.py
@@ -4,7 +4,7 @@ from random import randint
 from time import sleep
 
 
-class Table():
+class Table:
     def __init__(self, num):
         self.number = num
         self.guest = None
@@ -17,31 +17,47 @@ class Guest(threading.Thread):
     def run(self):
         sleep(randint(3, 10))
 
-class Cafe():
+
+class Cafe:
     def __init__(self, *args):
         self.queue = Queue()
         self.tables = args
 
     def guest_arrival(self, *guests):
-
-        for gs in guests:
-            for tbl in self.tables:
+        for tbl in self.tables:
+            for gs in guests:
                 if not tbl.guest:
-                   tbl.guest = gs.name
-                   print(f'{gs.name} сел(-а) за стол номер {tbl.number}')
-                   guests = tuple(item for item in guests if item.name != gs.name)
-                   gs.start()
-                   break
-        #print(guests)
-        for gs in guests:
-            self.queue.put(gs)
-            print(f'{gs.name} в очереди')
+                    tbl.guest = gs.name
+                    print(f'{gs.name} сел(-а) за стол номер {tbl.number}')
+                    guests = tuple(item for item in guests if item.name != gs.name)
+                    gs.start()
+
+        for guest in guests:
+            self.queue.put(guest)
+            print(f'{guest.name} в очереди')
 
 
 
     def discuss_guest(self):
-        print(self.queue.empty())
-        pass
+        while self.queue.qsize() > 0:
+            i = 0;
+            print(f'======={self.queue.qsize()}======')
+            for t in tables:# только занятые столы
+                #print(f'table:{t.number} guest: {t.guest}')
+                for g in guests:# все гости
+                    #print(f'cur{g.name}')
+                    if g.name == t.guest:# этот гость за столом
+                        #print(f'check{g.name} live:{g.is_alive()}')
+                        if not g.is_alive():# thread гостя умер
+                            #print(t.guest,t.number,g.name,g.is_alive())
+                            print(f'{g.name} покушал(-а) и ушёл(ушла)')
+                            print(f'Стол номер {t.number} свободен')
+                            que_guest = self.queue.get()# Берем гостя из очереди
+                            print(f'{que_guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {t.number}')
+                            que_guest.start()
+
+        print('end',self.queue.qsize())
+
 guests_names = ['Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman','Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra']
 tables = [Table(number) for number in range(1, 6)]
 print(tables)
