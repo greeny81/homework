0187e9f33e52a5e572ce3c4573ef593b98745f4f
Andy <greenyplay81@gmail.com>
2025-01-21 17:21:04 +0300

    mod_11_pract

--- module_11/mod_11_practice.py
+++ module_11/mod_11_practice.py
@@ -1,9 +1,20 @@
 from PIL import Image
-im = Image.open('rf.jpg')
-print(im.format, im.size, im.mode)
-#im.show()
 
-out = im.resize((256, 256))
-out.show()
+#print(im.format, im.size, im.mode)
+#im.show()
+def imRes(photo):
+    im = Image.open(photo)
+    imWidth, imHeight = im.size
+    return im.resize((imWidth//2, imHeight//2))
+
+im1 = imRes('rf.jpg')
+#im1.show()
+
+im2 = imRes('Снимок.JPG')
+
+im1.paste(im2, (50,20))
+im1.show('ggwp')
+
+
 
 
